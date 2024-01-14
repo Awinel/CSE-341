82ac306930bb5fbc43f9d2dd7c3a0e7617882db6
Awinel <benjamin.huerta.awi@gmail.com>
2024-01-13 21:08:12 -0300

    test01

--- week01/backend/index.js
+++ week01/backend/index.js
@@ -1,17 +1,28 @@
 const express = require('express');
-const bodyParser = require("body-parser");
-const { MongoClient } = require("mongodb");
+const bodyParser = require('body-parser');
+const MongoClient = require('mongodb').MongoClient;
+const mongodb = require('./db/connection');
+const professionalRoutes = require('./routes/professional');
 
-const app = express();
 const port = process.env.PORT || 8080;
- 
-app.use(express.static("public"));
+const app = express();
 
-app.get("/api/professional", (req, res) => {
-  res.json(profesionalData);
-});
+app
+  .use(bodyParser.json())
+  .use((req, res, next) => {
+    res.setHeader('Access-Control-Allow-Origin', '*');
+    next();
+  })
+  .use('/professional', professionalRoutes);
+app.get("/", (req, res) => {
+  res.send("Working..")
+})
 
- 
-app.listen(port, () => {
-  console.log(`Web Server is listening at port ${port}`);
+mongodb.initDb((err, mongodb) => {
+  if (err) {
+    console.log(err);
+  } else {
+    app.listen(port);
+    console.log(`Connected to DB and listening on ${port}`);
+  }
 });
