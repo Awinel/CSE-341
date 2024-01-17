de46841ee9ea86a74b06ab5ae002e7052c3c1df5
Awinel <benjamin.huerta.awi@gmail.com>
2024-01-16 21:22:38 -0300

    Just testing

--- week01/backend/db/connection.js
+++ week01/backend/db/connection.js
@@ -2,14 +2,14 @@ const dotenv = require('dotenv');
 dotenv.config();
 const MongoClient = require('mongodb').MongoClient;
 
-let _db = `mongodb+srv://${process.env.DB_USERNAME}:${process.env.DB_PASSWORD}@cluster0.22cgg98.mongodb.net/?retryWrites=true&w=majority`
+let _db;
 
 const initDb = (callback) => {
   if (_db) {
     console.log('Db is already initialized!');
     return callback(null, _db);
   }
-  MongoClient.connect(process.env._db)
+  MongoClient.connect(process.env.MONGODB_URI)
     .then((client) => {
       _db = client;
       callback(null, _db);
